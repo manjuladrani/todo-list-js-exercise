dadb7a16e4e5ee6953f358ae826467230afb279b
manjuladrani <88101950+manjuladrani@users.noreply.github.com>
2021-09-24 19:28:42 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,77 @@
+// Arrays to keep track of each task's state
+const taskTitles = [];
+const taskComplete = [];
+const taskDescription = [];
+// Create a new task by adding to the arrays
+// A new task will be created as incomplete
+function newTask(title) {
+  taskTitles.push(title);
+  taskComplete.push(false);
+}
+
+// Mark a task as complete by setting the task's status in the `taskComplete` array to `true`
+// function completeTask(taskIndex) {
+//   taskComplete[taskIndex] = true;
+// }
+
+function completeTask(task) {
+  task.complete = true;
+}
+
+// Print the state of a task to the console in a nice readable way
+// function logTaskState(taskIndex) {
+//   const title = taskTitles[taskIndex];
+//   const complete = taskComplete[taskIndex];
+//   console.log(`${title} has${complete ? " " : " not "}been completed`);
+// }
+
+function logTaskState(task) {
+  console.log(`${task.title}has ${task.complete ? " " : " not "}been completed`);
+}
+
+// function newTask(title, description) {
+//   taskTitles.push(taskTitle);
+//   taskDescription.push(description);
+//   taskComplete.push(false);
+// }
+
+
+function newTask(title, description) {
+  const task = {
+    title: title,
+    description: description,
+    complete: false,
+
+    logstate: function() {
+      console.log(`${this.title} has ${this.complete ? " " : " not "}been completed`);
+    },
+
+    markCompleted: function() {
+      this.complete = true;
+    }
+
+
+  };
+  return task;
+
+}
+// DRIVER CODE BELOW
+
+const task1 = newTask("Clean Cat Litter", "Take all the 💩 out of the litter box");
+const task2 = newTask("Do Laundry", "😨");
+const tasks = [task1, task2];
+
+// newTask("Clean Cat Litter"); // task 0
+// newTask("Do Laundry"); // task 1
+
+// logTaskState(task1); // Clean Cat Litter has not been completed
+// completeTask(task1);
+// logTaskState(task1); // Clean Cat Litter has been completed
+// using methods insted of functions
+task1.logstate();  // Clean Cat Litter has not been completed
+task1.markCompleted();
+task1.logstate();  // Clean Cat Litter has been completed
+
+
+// for now, let's just make sure we see our tasks
+console.log(tasks);
